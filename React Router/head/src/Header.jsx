1718b951ec0b82745f8c39e271a14bd8eb6db426
Eduardo <dududeoliveirakramer23@gmail.com>
2025-03-11 15:26:09 -0300

    novas atualizações

---
+++ React Router/head/src/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Header.css';
+import { NavLink, useLocation } from 'react-router-dom';
+
+const Header = () => {
+  const location = useLocation();
+  return (
+    <nav>
+      <NavLink to="/" activeStyle={{ color: 'tomato' }} end>
+        Home
+      </NavLink>{' '}
+      |{' '}
+      <NavLink activeStyle={{ color: 'tomato' }} to="Sobre">
+        Sobre
+      </NavLink>{' '}
+      |{' '}
+
+    </nav>
+  );
+};
+
+export default Header;
